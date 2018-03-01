b3a47fa1a46b04a300b93132965a451efd5491d4
Peter Lee <peterleex@hotmail.com>
2018-03-01 21:44:59 +0800

    a

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -6,16 +6,16 @@ using Microsoft.AppCenter.Crashes;
 
 namespace SQLiteNETSharedProject.iOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-	namespace FormsExample.iOS
-	{
-		[Register("AppDelegate")]
-		public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-		{
-			public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-			{
-				global::Xamarin.Forms.Forms.Init();
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    namespace FormsExample.iOS
+    {
+        [Register("AppDelegate")]
+        public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+        {
+            public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+            {
+                global::Xamarin.Forms.Forms.Init();
 
                 AppCenter.Start("ae827c97-7c00-4fe2-8092-6fdbaa84d288",
                    typeof(Analytics), typeof(Crashes));
@@ -24,11 +24,17 @@ namespace SQLiteNETSharedProject.iOS
 
                 Analytics.TrackEvent("SQLite App about to be started! 哈哈哈");
 
+                // Newer version of Visual Studio for Mac and Visual Studio provide the
+                // ENABLE_TEST_CLOUD compiler directive in the Debug configuration,
+                // but not the Release configuration.
+                #if ENABLE_TEST_CLOUD
+                    Xamarin.Calabash.Start();
+                #endif
 
                 return base.FinishedLaunching(app, options);
-			}
-		}
-	}
+            }
+        }
+    }
 
 }
 
