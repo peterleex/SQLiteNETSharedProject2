9aaef3a97ad6f1059867a66e0bb981f9ba49769a
Peter Lee <peterleex@hotmail.com>
2018-03-01 17:09:20 +0800

    First Commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace SQLiteNETSharedProject.Droid
+{
+	[Activity (Label = "SQLiteNETSharedProject.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            AppCenter.Start("ee581d3b-8e2a-4641-906d-ff06fca8aeca",
+                   typeof(Analytics), typeof(Crashes));
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
+
